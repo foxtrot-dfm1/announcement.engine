f9e149a7c46d56b221f46303aa1b32ed37ae7ada
Roman <kysilroman99@gmail.com>
2022-06-29 21:11:48 +0200

    Add Italian locale 🤌

--- src/announcement/engine/events/handlers.py
+++ src/announcement/engine/events/handlers.py
@@ -1,15 +1,28 @@
 """event handlers for announcmeent.engine package"""
 
 from plone import api
+from announcement.engine import _
 
+ANNOUNCEMENT_TRANSITION_MSG_MAP = {
+    _('publish'): _(
+        'announcement_publish_message',
+        default="Announcement was published, see"
+        ),
+    _('reject'): _(
+        'announcement_reject_message',
+        default="Announcement was rejected, see"
+        ) 
+}
 
 def noitifyAboutPublishReject(announcement, event):
     """
     Notifies announcement creator about publish/reject transition
     """
     transition_id = event.action
+    lang = api.portal.get_registry_record('plone.default_language')
+    translator = api.portal.get_tool('translation_service')
 
-    if transition_id not in ('publish', 'reject'):
+    if transition_id not in (ANNOUNCEMENT_TRANSITION_MSG_MAP.keys()):
         return False
 
     recipient = api.user.get(
@@ -19,8 +32,15 @@ def noitifyAboutPublishReject(announcement, event):
     if not recipient:
         return False
 
-    subject = f"{announcement.title} ::> {transition_id + 'ed'}"
-    message = f"Announcment was {transition_id + 'ed'}, see: {announcement.absolute_url()}"
+    # If u have any idea how to refactor this, text me <kysilroman99@gmail.com> ☃
+    subject = f"""{announcement.title} ::> {translator.translate(
+            _(transition_id), target_language=lang, default=f"{transition_id + 'ed'}"
+        )}"""
+
+    message = f"""{translator.translate(
+            ANNOUNCEMENT_TRANSITION_MSG_MAP[transition_id],
+            target_language=lang
+        )} : {announcement.absolute_url()}"""
 
     api.portal.send_email(
         recipient=recipient,
