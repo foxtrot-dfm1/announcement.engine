6d5f137f72ed86dcc7f0e15286a242ff7240f9d8
Roman <kysilroman99@gmail.com>
2022-07-07 16:45:57 +0200

    Add view test

---
+++ src/announcement/engine/tests/test_views.py
@@ -0,0 +1,32 @@
+#Pythonic libraries
+import unittest
+
+#Plone
+from plone import api
+from plone.app.testing import TEST_USER_ID
+from plone.app.testing import setRoles
+
+from announcement.engine.testing import \
+    ANNOUNCEMEMNT_ENGINE_INTEGRATION_TESTING
+
+
+class TestAnnouncementCreateView(unittest.TestCase):
+    layer = ANNOUNCEMEMNT_ENGINE_INTEGRATION_TESTING
+
+    def setUp(self):
+        self.portal = self.layer['portal']
+        self.request = self.layer['request']
+        
+        setRoles(self.portal, TEST_USER_ID, ['Manager'])
+        self.test_item = api.content.create(self.portal, 'announcement',
+                                            title='announcement_test')
+    
+    def test_view_contains_text(self):
+        view = api.content.get_view(
+            name='announcement_creation_template',
+            context=self.test_item,
+            request=self.request
+        )
+
+        self.assertIn(self.test_item.title, view(announcement=self.test_item))
+        self.assertIn(self.test_item.absolute_url(), view(announcement=self.test_item))
